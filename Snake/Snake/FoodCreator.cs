625b66d889df4ea6201398fcb58bd2406af3241e
potato <hamadahero922@gmail.com>
2020-09-03 15:04:12 +0600

    Food

---
+++ Snake/Snake/FoodCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Snake
+{
+    class FoodCreator
+    {
+        int mapWidth, mapHeight;
+        char sym;
+
+        Random random = new Random();
+
+        public FoodCreator(int mapWidth, int mapHeight, char sym)
+        {
+            this.mapHeight = mapHeight;
+            this.mapWidth = mapWidth;
+            this.sym = sym;
+        }
+
+        public Point CreateFood()
+        {
+            int x = random.Next(2, mapWidth - 2);
+            int y = random.Next(2, mapHeight - 2);
+            return new Point(x, y, sym);
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -24,8 +24,24 @@ namespace Snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
 
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while (true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
